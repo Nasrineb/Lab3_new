a251380d0d36f54bf051a94b2b1ed32ac3e67bfc
Nasrineb <nasrin.ebadi@gmail.com>
2022-11-21 15:15:12 +0100

    hi

---
+++ connection.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose');
+
+// Connection URL
+const url = 'mongodb+srv://nasrin:1392@nasrin.ykolwlc.mongodb.net/Library';
+
+async function main() {
+    // Use connect method to connect to the server
+    console.log('Connected successfully to server');
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    const mgDB = mongoose.connection;
+    mgDB.on('connected', console.log.bind(console, 'MongoDB & Mongoose Connected'));
+}
+
+module.exports = main;
